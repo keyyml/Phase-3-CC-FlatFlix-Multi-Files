2937a9a2d48d67c81b51c00a3952612fc39c6b4b
Eleanor Kelman <ekelman3@gmail.com>
2023-10-11 15:47:40 -0400

    set up CC

---
+++ lib/classes/Movie.py
@@ -0,0 +1,12 @@
+class Movie:
+    def __init__(self, title):
+        self.title = title
+
+    def reviews(self):
+        pass
+
+    def reviewers(self):
+        pass
+
+    def average_rating(self):
+        pass
---
+++ lib/classes/Review.py
@@ -0,0 +1,5 @@
+class Review:
+    def __init__(self, viewer, movie, rating):
+        self.viewer = viewer
+        self.movie = movie
+        self.rating = rating
